ee86d5d3d1eddf9d531817466130fb57506b8bd2
Satu Koskinen <satu.a.koskinen@gmail.com>
2022-09-20 00:19:20 +0300

    Add baranasi albert graph and use that in testing page rank

--- examples/page-rank.rs
+++ examples/page-rank.rs
@@ -10,6 +10,11 @@ use gdsl::*;
 use std::cell::Cell;
 use std::rc::Rc;
 use gdsl::digraph as graph;
+use std::fs::File;
+use std::io::Write;
+
+mod barabasi_albert_graph;
+use barabasi_albert_graph::barabasi_albert_random_graph;
 
 /// Types definitions for the graph and nodes.
 type N = Node<usize, Cell<f64>, Rc<Cell<f64>>>;
@@ -75,7 +80,8 @@ pub fn page_rank(g: &G, α: f64, ε: f64) {
 	}
 }
 
-fn main() {
+#[test]
+fn test() {
     let mut g = graph![
 		(usize, Cell<f64>) => [Rc<Cell<f64>>]
 		(0, Cell::new(0.0)) => [(1, Rc::new(Cell::new(1.0))), (2, Rc::new(Cell::new(2.0)))]
@@ -92,4 +98,63 @@ fn main() {
 	assert!(g[1].get() == 0.16887946089722788);
 	assert!(g[2].get() == 0.3295188106005262);
 	assert!(g[3].get() == 0.1517727517937135);
+}
+
+fn attr(field: &str, value: &str) -> (String, String) {
+	(field.to_string(), value.to_string())
+}
+
+pub const THEME: [&str; 5] = [
+	"#ffffff", // 0. background
+	"#ffe5a9", // 1. medium
+	"#423f3b", // 2. dark
+	"#ff6666", // 3. accent
+	"#525266", // 4. theme
+];
+
+fn main() {
+	let g = barabasi_albert_random_graph(50, 5);
+	let mut v = g.to_vec();
+	page_rank(&v, 0.85, 0.0001);
+	v.sort_by(|l, r| l.key().cmp(r.key()));
+	for node in &v {
+		println!("{}: {}", node.key(), node.get());
+	}
+	let sum = v.iter().map(|n| n.get()).sum::<f64>();
+	println!("Sum: {}", sum);
+
+	// Write a dot representation with page rank * 100 as node size.
+	let dot_str = g.to_dot_with_attr(
+		&|graph| {
+			let graph_size_mb = graph.sizeof() as f64 / 1024.0 / 1024.0;
+			Some(vec![
+				attr("bgcolor", THEME[0]),
+				attr("fontcolor", THEME[4]),
+				attr("label", &format!("Barabasi Albert Graph {:.4} Mb", graph_size_mb)),
+			])
+		},
+		&|node| {
+			let color = THEME[3];
+			let size = node.get() * 10.0;
+			Some(vec![
+				attr("color", color),
+				attr("fontcolor", THEME[4]),
+				attr("label", &format!("{}", node.key())),
+				attr("width", &format!("{}", size)),
+				attr("height", &format!("{}", size)),
+			])
+		},
+		&|_, _, edge| {
+			let color = THEME[4];
+			Some(vec![
+				attr("fontcolor", THEME[4]),
+				attr("weight", &edge.get().to_string()),
+				attr("color", color),
+			])
+		},
+	);
+
+	// Write the dot file to disk.
+	let mut file = File::create("graph.dot").unwrap();
+	file.write_all(dot_str.as_bytes()).unwrap();
 }
